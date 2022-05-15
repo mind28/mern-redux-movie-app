bef1a359e0a11a64f71a5c810c48d16fe8e9c1d8
mind28 <96925869+mind28@users.noreply.github.com>
2022-05-15 18:41:42 -0400

    Step 1

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+app.get('/', (req,res) =>{
+    res.send('Hello World')
+})
+
+app.listen(PORT, () => console.log(`Server started od PORT: ${PORT}`))
+
+
