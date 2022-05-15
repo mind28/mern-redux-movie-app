f7596b3f6ba872e21e166164380ae7c6cfeddb56
mind28 <96925869+mind28@users.noreply.github.com>
2022-05-15 19:07:59 -0400

    Step 2

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,11 @@ const PORT = process.env.PORT || 8000
 const app = express()
 
 app.get('/', (req,res) =>{
-    res.send('Hello World')
+    res.status(200).json({message: 'Welcome to MovieShare'})
 })
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.listen(PORT, () => console.log(`Server started od PORT: ${PORT}`))
 
